58ae1db49626068aab0af4470e0686134ef99001
Abdullah-Razzaq <abdullahrazzaq004@gmail.com>
2023-09-18 16:33:55 +0500

    added files

---
+++ lab_task1/Main.java
@@ -0,0 +1,10 @@
+package lab_task1;
+
+public class Main {
+    public static void main(String[] args) {
+        Music m1 = new Music("black","4:00","sad",new Date(23,12,2016),new Singer("alanWalker","male","american",new Date(13,5,2011)));
+
+        System.out.println(m1);
+
+    }
+}
